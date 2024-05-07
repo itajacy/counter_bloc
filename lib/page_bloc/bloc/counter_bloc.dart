81fcbc21ac258f2f6bdebd646f612de29f0e1bb8
itajacy TI02 <itajacy@bsd.com.br>
2024-05-07 13:29:17 -0300

    estrutura basica do Bloc

--- lib/page_bloc/bloc/counter_bloc.dart
+++ lib/page_bloc/bloc/counter_bloc.dart
@@ -3,4 +3,11 @@ import 'package:bloc/bloc.dart';
 part 'counter_event.dart';
 part 'counter_state.dart';
 
-class CounterBloc extends Bloc<CounterEvent, CounterState> {}
+//* No Bloc sao criados 3 arquivos que trabalham em conjunto bloc, event, state
+//* por isso o 'part' e o 'part of', pois na verdade seriam uma coisa só
+//* mas que são separadas para melhor organização
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  //* estado inicial da aplicação
+  CounterBloc() : super(CounterStateInitial());
+}
--- lib/page_bloc/bloc/counter_state.dart
+++ lib/page_bloc/bloc/counter_state.dart
@@ -1,3 +1,17 @@
 part of 'counter_bloc.dart';
 
-abstract class CounterState {}
+//*  No Bloc o 'estado' de tudo é uma coisa só, e não por atributos separados
+//*  TUDO É ATUALIZADO DE UMA ÚNICA VEZ
+
+//* PQ A CLASSE SER ABSTRATA?
+
+abstract class CounterState {
+  final int counter;
+
+  CounterState(this.counter);
+}
+
+class CounterStateInitial extends CounterState {
+  //* O estado inicial sempre será 0(ZERO)
+  CounterStateInitial() : super(0);
+}
